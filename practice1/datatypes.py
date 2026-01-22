f08029b998d74d9e68c8e4e6f72ece9dd2f44b65
Aigerim <bakbergenaigerim13@gmail.com>
2026-01-22 18:54:34 +0500

    add data types examples

---
+++ practice1/datatypes.py
@@ -0,0 +1,11 @@
+#different data types in python
+x=20 #integer
+y=5.875 #float
+z="Aigerim" #string 
+answer=False #boolean
+
+#printing the type of each veriable 
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(answer))
