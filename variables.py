7a476a0b76ac74317d87db190d037bac14578342
Aigerim <bakbergenaigerim13@gmail.com>
2026-01-22 18:23:59 +0500

    add variables examples

---
+++ variables.py
@@ -0,0 +1,20 @@
+#first example 
+x=10
+y="Aigerim"
+print(x)
+print(y)
+print()
+
+#second example 
+a=99
+a="KBTU"
+print(a)
+print()
+
+#third example 
+u=str(888)
+v=int(888)
+d=float(888)
+print(u)
+print(v)
+print(d)
